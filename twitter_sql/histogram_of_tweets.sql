47bb48a666ee356afcd792f2f6a1df3ebe4ebf34
Dhosti Chakradar Reddy <reddynagi914@gmail.com>
2026-01-22 09:41:57 +0530

    Add solution for histogram of tweets SQL problem

--- twitter_sql/histogram_of_tweets.sql
+++ twitter_sql/histogram_of_tweets.sql
@@ -1,1 +1,18 @@
+-- Histogram of Tweets (Twitter SQL Interview Question)
+-- This query creates a histogram showing how many users
+-- posted a certain number of tweets in 2022.
+
+SELECT
+    tweet_count AS tweet_bucket,
+    COUNT(*) AS users_num
+FROM (
+    SELECT
+        user_id,
+        COUNT(*) AS tweet_count
+    FROM tweets
+    WHERE EXTRACT(YEAR FROM tweet_date) = 2022
+    GROUP BY user_id
+) t
+GROUP BY tweet_count
+ORDER BY tweet_bucket;
 
